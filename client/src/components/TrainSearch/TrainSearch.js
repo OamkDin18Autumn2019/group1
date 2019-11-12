09512ae5ebdb97d275ae3931d220d03e46fe9885
Joel <joel.m.maenpaa@gmail.com>
2019-11-13 00:42:12 +0200

    select and input handlers, input handler subject to change

--- client/src/components/TrainSearch/TrainSearch.js
+++ client/src/components/TrainSearch/TrainSearch.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
 
+// import components
 import Dropdown from './Dropdown/Dropdown'
 import Input from './Input/Input'
 
 class TrainSearch extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             input: "",
             select: "destination",
         }
     }
 
+    // select change handler
+    handleSelectChange = (e) => {
+        this.setState({ select: e.target.value })
+    }
+
+    // input change handler
+    handleInputChange = (e) => {
+        this.setState({ input: e.target.value })
+    }
+
     render() {
         return (
             <div>
-                <Dropdown value={this.state.select} />
-                <Input value={this.state.input} />
+                <Dropdown value={this.state.select} onChange={this.handleSelectChange} />
+                <Input value={this.state.input} onChange={this.handleInputChange} />
             </div>
         )
     }
