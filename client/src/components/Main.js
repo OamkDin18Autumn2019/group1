9441e31f0d83f1cfe5768c4abf396cfb968107be
Joel <joel.m.maenpaa@gmail.com>
2019-11-13 00:38:46 +0200

    fetch for all live trains

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,7 +3,29 @@ import React, { Component } from 'react'
 import TrainSearch from './TrainSearch/TrainSearch'
 
 class Main extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: [],
+            isLoading: true
+        }
+    }
+
+    // fetch all live trains
+    componentDidMount() {
+        fetch('https://rata.digitraffic.fi/api/v1/live-trains')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ data: data, isLoading:false })
+                console.log(this.state.data)
+            })
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return "Loading..."
+        }
+
         return (
             <div>
                 <TrainSearch />
