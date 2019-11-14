7bf0cfd997d9cd1c533f72263159b0e5fb9b3829
Joel Mäenpää <zoloppe4lyf@gmail.com>
2019-11-14 19:06:55 +0200

    dropdown is now a class component, made a function to map() all available trains

--- client/src/components/TrainSearch/Dropdown/Dropdown.js
+++ client/src/components/TrainSearch/Dropdown/Dropdown.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Dropdown(props) {
-    console.log(props.data)
-    return (
-        <select value={props.select} onChange={props.onChange}>
-            <option value="destination">Destination</option>
-        </select>
-    )
+class Dropdown extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: this.props.data
+        }
+    }
+    // option map function for dropdown
+    optionMap = () => {
+        return this.state.data.map((number, index) => {
+            return <option key={index} value={number.trainNumber}>{number.trainNumber}</option>
+        })
+    }
+
+    render() {
+        return (
+            <select value={this.props.select} onChange={this.props.onChange}>
+                {this.optionMap()}
+            </select>
+        )
+    }
 }
 
 export default Dropdown
