2c7b4972450531dcd3c4e9d720acc3bc9d4527dd
Minsi <k8jami00@students.oamk.fi>
2019-11-18 21:42:35 +0200

    Added registerview functionality

--- client/src/components/AppRouter/Views/RegisterView.js
+++ client/src/components/AppRouter/Views/RegisterView.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+
+function register(event) {
+  event.preventDefault();
+  
+}
+
 
 class RegisterView extends Component {
-    render() {
+
+  render() {
         return (
             <div>
+                <h2> User register</h2>
+      <div>Please enter your full name, username and password</div>
+      <br/>
+      <form onSubmit={ register }>
+        <div className="tableCellNoBorder">
+          <div className="tableRow">
+            <div className="tableCellNoBorder"> Full name: </div>
+            <div className="tableCellNoBorder"> <input type="text" name="full_name" /> </div>
+          </div>
+          <div className="tableRow">
+            <div className="tableCellNoBorder"> Username: </div>
+            <div className="tableCellNoBorder"> <input type="text" name="username" /> </div>
+          </div>
+          <div className="tableRow">
+            <div className="tableCellNoBorder"> Password: </div>
+            <div className="tableCellNoBorder"> <input type="password" name="password" /> </div>
+          </div>
+          <div className="tableRow">
+            <div className="tableCellNoBorder"> <br/>  <button type="submit">Register</button> </div>
+            <div className="tableCellNoBorder"> <br/>  <Link to="/"><button>Cancel</button></Link> </div>
+          </div>
+        </div>
+      </form>
                 
             </div>
         )
