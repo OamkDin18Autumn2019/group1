2e040f632256a8fe889069421dd545a2cb2ecf7d
HuyLee24 <mihawk240600@gmail.com>
2019-12-16 10:12:27 +0200

    little changes

--- client/src/components/AppRouter/Views/History.js
+++ client/src/components/AppRouter/Views/History.js
@@ -6,7 +6,7 @@ class History extends Component {
     render() {
         return(
             <div>
-                <h2 style={{textAlign: "center"}}>View your previous selection:</h2>
+                <h2 style={{textAlign: "center"}}>View your previous selections:</h2>
             </div>
           )
       }
